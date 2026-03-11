fcfe0c8f36cfffeb8680c7014aab91ef9d0f1d09
Sebastian Buritica <jhoseburitica40@gmail.com>
2026-03-10 23:13:40 -0500

    Está dando error

---
+++ src/main/java/com/customer/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.customer.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.customer.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    
+}

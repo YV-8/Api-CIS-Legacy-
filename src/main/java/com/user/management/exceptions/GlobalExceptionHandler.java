b92cf64f6c01bf62728073d9a3c60d85a5940fe7
Moni <monica.escobar0125@jala.university>
2026-03-14 19:30:50 -0400

    Modifice the exceptions for create users for manage 400 and 201

--- src/main/java/com/user/management/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/user/management/exceptions/GlobalExceptionHandler.java
@@ -2,14 +2,31 @@ package com.user.management.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
 
-@RestControllerAdvice
+@ControllerAdvice(annotations = RestController.class)
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleValidation(IllegalArgumentException ex) {
+        ErrorResponse error = new ErrorResponse(
+            400,
+            "Bad Request",
+            ex.getMessage()
+        );
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException ex) {
+        return buildResponse(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
+        ex.printStackTrace();
         return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Error interno del servidor");
     }
 

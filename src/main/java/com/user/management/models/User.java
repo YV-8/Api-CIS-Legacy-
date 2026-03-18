e6ec5d57c672f0103424e7c468cecac513d5b031
Pierre Crespin <Pierre.Crespin0125@jala.university>
2026-03-18 18:06:33 -0300

    feat(docu): Add doc the coexistence with legacy  CLI

--- src/main/java/com/user/management/models/User.java
+++ src/main/java/com/user/management/models/User.java
@@ -2,6 +2,8 @@ package com.user.management.models;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -18,6 +20,7 @@ import lombok.Setter;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id", length = 36, nullable = false, unique = true)
     private String id;
 

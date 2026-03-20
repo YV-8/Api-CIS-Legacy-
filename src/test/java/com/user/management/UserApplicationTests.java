39b41e9161bbabfdbc053128b9f65944d34ee26a
David Santos <David.Santos0724@jala.university>
2026-03-20 12:48:24 -0500

    update pipeline

--- src/test/java/com/user/management/UserApplicationTests.java
+++ src/test/java/com/user/management/UserApplicationTests.java
@@ -2,12 +2,13 @@ package com.user.management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UserApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }

28de5bdf89d2d06f5031fa1f1135da5159fb94aa
Pierre Crespin <Pierre.Crespin0125@jala.university>
2026-03-21 22:53:34 -0300

    resolved conflict

--- src/main/java/com/user/management/config/SecurityConfig.java
+++ src/main/java/com/user/management/config/SecurityConfig.java
@@ -1,14 +1,10 @@
 package com.user.management.config;
 
-import com.user.management.security.JwtAuthenticationFilter;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import lombok.RequiredArgsConstructor;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 
 import org.springframework.security.authentication.AuthenticationManager;
@@ -26,7 +22,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import jakarta.servlet.http.HttpServletResponse;
 

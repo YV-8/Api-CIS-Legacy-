892249e0123b7a80b605eef175b1a5d978d7ed96
Moni <monica.escobar0125@jala.university>
2026-03-22 14:01:06 -0400

    Modificate repository and delete missing imports

--- src/main/java/com/user/management/services/UserService.java
+++ src/main/java/com/user/management/services/UserService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;

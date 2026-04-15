bb7924140703d8fc1ebfa0aac28fb84e3e97208f
Moni <monica.escobar0125@jala.university>
2026-04-15 09:45:03 -0400

    Implement most votes to controlls

---
+++ CIS.Api/Controllers/StatsController.cs
@@ -0,0 +1,41 @@
+using CIS.BusinessLogic.Services;
+using Microsoft.AspNetCore.Mvc;
+using CIS.BusinessLogic.Exceptions;
+
+[ApiController]
+[Route("api/v1/stats")]
+public class StatsController : ControllerBase
+{
+    private readonly IIdeaService _ideaService;
+    public StatsController(IIdeaService ideaService)
+    {
+        _ideaService = ideaService;
+    }
+
+    [HttpGet("ideas/top")]
+    public async Task<IActionResult> GetTopIdeas([FromQuery] string? topicId, [FromQuery] int limit = 200)
+    {
+        var topIdeas = await _ideaService.GetTopIdeasAsync(topicId, limit);
+        return Ok(topIdeas);
+    }
+    [HttpGet("topics/{topicId}/ideas/top")]
+    public async Task<IActionResult> GetTopIdeasByTopic([FromRoute] string topicId, [FromQuery] int limit = 200)
+    {
+        try 
+        {
+            var result = await _ideaService.GetTopIdeasAsync(topicId, limit: 200);
+            return Ok(result); // Si no hay ideas, retorna [] automáticamente por el ToListAsync
+        }
+        catch (NotFoundException ex)
+        {
+            return NotFound(new { message = ex.Message });
+        }
+    }
+
+    [HttpGet("users/top")]
+    public async Task<IActionResult> GetTopUsers([FromQuery] int limit = 200)
+    {
+        var topUsers = await _ideaService.GetTopUsersAsync(limit);
+        return Ok(topUsers);
+    }
+}  

f8f7a6a934a06eef0b67a3e79926e59ab395833c
Pierre Crespin <Pierre.Crespin0125@jala.university>
2026-04-25 18:42:34 -0300

    feat(repository): add MongoDB module with UserDocument and MongoUserRepositoryAdapter - Create repository/mongo package with UserDocument, UserMongoRepository and adapter - MongoUserRepositoryAdapter active on profile "mongodb" only - UUID assigned in adapter before save (no MongoDB ObjectId dependency) - No @Transactional: single-document ops are atomic by default in MongoDB - Private toDocument/toUser mappers keep infrastructure isolated from domain

---
+++ src/main/java/com/user/management/models/UserDocument.java
@@ -0,0 +1,35 @@
+package com.user.management.repository.mongo;
+
+import com.user.management.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDocument {
+
+    @Id
+    private String id;
+
+    @Field("name")
+    private String name;
+
+    @Indexed(unique = true)
+    @Field("login")
+    private String login;
+
+    @Field("password")
+    private String password;
+
+    @Field("role")
+    private Role role;
+}
